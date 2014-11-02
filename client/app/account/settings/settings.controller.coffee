lsiden/cid-farmersmarket

commit 9ab1806c01f6517c982e107e58646c68da352493

/ and /events - Comply with wireframe
/ - Redid front page to follow wireframe

Took a bit of creative license with the copy.  ;)

/, /events - front page

fixed broken test

/ added Angular UI Calendar

karma conf

/, /events - styling

client/app/account/settings/settings.controller.coffee
@@ -88,6 +88,7 @@ angular.module 'farmersmarketApp'
 .directive 'matchPassword', ->
   {
     require: 'ngModel'
+    restrict: 'E'
     controller: 'ChangePasswordCtrl'
     link: (scope, el, attrs, model) ->
       model.$validators.matchPassword = (modelValue, viewValue) ->

client/app/app.coffee
@@ -6,7 +6,8 @@ angular.module 'farmersmarketApp', [
   'ngSanitize',
   'angular-flash.service',
   'angular-flash.flash-alert-directive',
-  'ngGrid'
+  'ui.calendar',
+  'ngGrid',
   'ui.router',
   'ui.bootstrap',
   'ui.mask'

client/app/app.scss
@@ -31,6 +31,15 @@ input.ng-invalid {
 div[flash-alert] {
   display: none;
 }
+
 div[flash-alert].in {
   display: block;
+}
+
+.float-right {
+  float: right;
+}
+
+.event-summary > .row {
+  border: thin solid black;
 }

client/app/events/eventSummary.jade
@@ -1,0 +1,19 @@
+.event-summary
+  //- {{ event }}
+  .row
+    .col-sm-4.first
+      .row
+        | {{ event.name }}
+      .row
+        | {{ event.sponsor }}
+      .row
+        | Need {{ event.volunteerSlots }} volunteers.
+    .col-sm-4
+      .row
+        | {{event.date}}
+      .row
+        | {{ event.starts }} - {{ event.ends }}
+      .row
+        | {{ event.volunteers|decorateNumVolunteers }} signed up.
+    .col-sm-4
+      button.btn.btn-primary.volunteer(type='button') + Volunteer for this Event

client/app/events/events.controller.coffee
@@ -1,28 +1,49 @@
 'use strict'
 
-angular.module 'farmersmarketApp'
-.controller 'EventsCtrl', ['$scope', '$http', 'Event', ($scope, $http, Event) ->
+m = angular.module 'farmersmarketApp'
+
+m.controller 'EventsCtrl', ['$scope', '$http', 'Event', ($scope, $http, Event) ->
   $scope.errors = {}
+  $scope.events = []
 
-  # http://stackoverflow.com/a/24082603/270511
-  $scope.gridData = {}
-  $scope.gridOptions = { data: 'gridData' }
-
-  makeGridEvent = (event) ->
-    gridEvent =
-      Event: event.name
-      Date: (new Date(event.start)).toDateString()
-      Starts: (new Date(event.start)).shortTime()
-      Ends: (new Date(event.end)).shortTime()
-      # active: event.active # TODO make this available to admin only
-
-    gridEvent
+  # For Angular ui.calendar widget
+  calEvents = []
+  $scope.calEventSources = [events: calEvents]
 
   # Request all events that haven't ended
   # TODO - show only active events for visitors.
-  query = { end: '>' + (new Date()).addDays(-1) };
+  query = { end: '>' + (new Date()).addDays(-1) }
   # console.log(query);
-  Event.get query, (events) ->
-    $scope.gridData = (makeGridEvent(event) for event in events)
 
-]
+  makeEventItem = (event) ->
+    name: event.name
+    date: (new Date(event.start)).toDateString()
+    starts: (new Date(event.start)).shortTime()
+    ends: (new Date(event.end)).shortTime()
+    volunteers: event.volunteers
+    volunteerSlots: event.volunteerSlots
+    sponsor: event.sponsor
+
+  makeCalendarEventItem = (event) ->
+    title: event.name
+    start: event.start
+
+  Event.get query, (events) ->
+    $scope.events = (makeEventItem event for event in events)
+    calEvents.length = 0
+
+    for event in events
+      calEvents.push makeCalendarEventItem(event)
+  ]
+
+m.directive 'eventSummary', ->
+  restrict: 'E'
+  scope:
+    event: '='
+  templateUrl: 'app/events/eventSummary.html'
+
+m.filter 'decorateNumVolunteers', ->
+  (num) ->
+    if (num == 0)
+      return 'None'
+    num

client/app/events/events.jade
@@ -2,9 +2,7 @@ div(ng-include='"components/navbar/navbar.html"')
 .container
 
   .h1 Upcoming Events
-  
-  .row
-    .col-md-12(ng-controller='EventsCtrl')
-      //- {{ events }}
-      //- {{ gridOptions }}
-      .ngGrid(ng-grid="gridOptions")
+
+  div(ng-controller='EventsCtrl')
+    div(ng-repeat='event in events track by $index')
+      event-summary(event='event')

client/app/events/events.scss
@@ -1,3 +1,8 @@
 .ngGrid .ngViewport {
   height: 100% !important; // https://github.com/angular-ui/ng-grid/issues/80#issuecomment-39466906
 }
+
+button.volunteer {
+  margin: 0.25cm 0;
+  float: right;
+}

client/app/main/main.controller.coffee
@@ -2,18 +2,9 @@
 
 angular.module 'farmersmarketApp'
 .controller 'MainCtrl', ($scope, $http) ->
-  $scope.awesomeThings = []
 
-  $http.get('/api/things').success (awesomeThings) ->
-    $scope.awesomeThings = awesomeThings
-    
-
-  $scope.addThing = ->
-    return if $scope.newThing is ''
-    $http.post '/api/things',
-      name: $scope.newThing
-
-    $scope.newThing = ''
-
-  $scope.deleteThing = (thing) ->
-    $http.delete '/api/things/' + thing._id
+  ###  
+    $scope.uiConfig =
+      calendar:
+        height: 100
+  ###

commit 38a8cb45590129bfbaa3cdfea56360b5cb3c7e14

/settings - Fixed broken password re-type validation
Broke that in commit 9ab1806c

client/app/account/settings/settings.controller.coffee
@@ -88,7 +88,7 @@ angular.module 'farmersmarketApp'
 .directive 'matchPassword', ->
   {
     require: 'ngModel'
-    restrict: 'E'
+    restrict: 'A'
     controller: 'ChangePasswordCtrl'
     link: (scope, el, attrs, model) ->
       model.$validators.matchPassword = (modelValue, viewValue) ->